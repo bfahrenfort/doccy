2455a6eb8d72f50f03b48635a357d3fe66685310
OCDkirby <59982409+OCDkirby@users.noreply.github.com>
2023-02-15 21:58:28 -0600

    Create pre-enable checks

---
+++ Doccy-EnableSgpt.ps1
@@ -0,0 +1,19 @@
+# Ensure that shell-gpt is installed before attempting to enable the module
+
+if (pip show shell-gpt)
+{
+  Write-Output 'Doccy-EnableSgpt: shell_gpt found.'
+  return 0
+}
+else
+{
+  $Confirmation = Read-Host "Doccy-EnableSgpt: shell_gpt not found. Should doccy install it?`n[y/N]"
+  if ($Confirmation -eq 'y')
+  {
+    if (pip install shell-gpt)
+    {
+      return 0
+    }
+  }
+}
+return 1
